865e487f989aa5f1a2c99b7a3ebf14c536c027ca
Nathan Sadler <nssadler@my.waketech.edu>
2021-02-02 11:46:12 -0500

    Rotues file reworking

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'articles#index'
   resources :widgets
+  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
   #root 'welcome#index'
-  get "/articles", to: "articles#index"
+  #get "/articles", to: "articles#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
