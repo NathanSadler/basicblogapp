310e8e0af5b6493b1cb261a730651b1558587fc6
Nathan Sadler <nssadler@my.waketech.edu>
2021-02-04 16:37:27 -0500

    Prettified code

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -62,13 +62,14 @@ class WidgetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_widget
-      @widget = Widget.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def widget_params
-      params.require(:widget).permit(:name, :description, :stock)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_widget
+    @widget = Widget.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def widget_params
+    params.require(:widget).permit(:name, :description, :stock)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
-
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
